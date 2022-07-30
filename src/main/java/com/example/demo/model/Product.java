89ae222815760860455e7658dbdcc586c139125b
Gadi Nahary <gnahary@gmail.com>
2022-07-30 12:12:20 +0300

    First commit

--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -1,6 +1,4 @@
 package com.example.demo.model;
 
-import com.example.demo.model.ProductCategory;
-
 public record Product(String title, ProductCategory productCategory, Float price, String serialId){}
 
